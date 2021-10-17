1e3a5cf6f9da8e2dd1b9a8c4fd82d21454f72929
Chirita Andrei <andrei.chirita2013@gmail.com>
2021-10-17 19:51:21 +0300

    Create Login and Signup for ZocDocClone App

---
+++ app/src/main/java/eu/ase/chirita_andrei/proiect/zocdocclone/MainActivity.java
@@ -0,0 +1,28 @@
+package eu.ase.chirita_andrei.proiect.zocdocclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCreateAccount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCreateAccount = findViewById(R.id.btn_main_create_an_account);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/eu/ase/chirita_andrei/proiect/zocdocclone/SignUpActivity.java
@@ -0,0 +1,28 @@
+package eu.ase.chirita_andrei.proiect.zocdocclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private TextView tvSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        tvSignIn = findViewById(R.id.tv_sign_up_activity_already_have);
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
